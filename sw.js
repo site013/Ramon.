3926b3b797820f6294b8c91c5cb94098c66e546a
site013 <andson7pontes08@gmail.com>
2026-02-27 22:08:24 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'nayma-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
